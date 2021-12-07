d5c09b4ef537eba0b0035547091de2ba13e7db8f
sato-takehiro <sato.takehiro00@gmail.com>
2021-12-07 15:35:58 +0900

    add Record.bat

---
+++ Record.bat
@@ -0,0 +1,6 @@
+:RecordファイルはDesktop上に設置
+net use R: \\DESKTOP-M7QQ2HO\Users\koishi\Desktop\Record
+R:
+C:\Users\koishi\AppData\Local\Programs\Python\Python39\python Record.py > Record.txt
+net use R: /delete /yes
+exit
